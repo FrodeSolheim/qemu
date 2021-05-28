982810b556571a9b7961d1a9a2f9451f1d839aaf
Frode Solheim <frode@solheim.dev>
2021-05-28 23:56:55 +0200

    Updated build scripts for ARM64

--- fsbuild/configure.sh
+++ fsbuild/configure.sh
@@ -4,8 +4,10 @@ set -e
 
 . fsbuild/system.sh
 
+cp VERSION.qemu VERSION
 if [ $SYSTEM_OS = "Windows" ]; then
 ./configure --with-system-pixman --python=python2 --static
 else
 ./configure --with-system-pixman --python=python2
 fi
+rm VERSION
